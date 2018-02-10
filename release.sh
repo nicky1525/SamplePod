6f2e05594f936140b5d04833b09fcfc9bba8848b
nicky1525 <nicky_1525@hotmail.it>
2018-02-10 15:41:06 +0000

    Update script

--- release.sh
+++ release.sh
@@ -1,18 +1,22 @@
 #!/usr/bin/env bash
 
+# exit on error
 set -e
 
+$branch=master
+$version=0.2.0
+
 git stash
-git checkout master
+git checkout $branch
+git pull --rebase
 git stash pop
 git add SamplePod.podspec
 git commit -m "Bump up pod version"
 cd Example
 pod install
 cd ..
-git add .
+git add Example
 git commit -m "Update sample app"
-git tag 0.1.1 -m "v0.1.1"
+git tag $version -m "v$version"
 git push --tags
 pod repo push SamplePod
-
